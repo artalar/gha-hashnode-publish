c9d57d0a0633ad41d83917cf382350e7f5c416ee
Arutyunyan Artyom <artalar@ya.ru>
2020-12-26 15:35:42 +0000

    add debug logs

--- index.ts
+++ index.ts
@@ -22,24 +22,28 @@ const PostsResp = t.Record({
 })
 const PostsByName = t.Dictionary(Post)
 
+const debug = (message: string) => core.debug(`[code] ${message}`)
+
 async function run() {
   try {
     const hashnodeAuth = core.getInput('hashnodeAuth', { required: true })
     const hashnodeUsername = core.getInput('hashnodeUsername', { required: true })
     const postsPath = core.getInput('postsPath') || 'posts'
     const { owner, repo } = github.context.repo
-
-    core.debug(`Clone repo`)
-
-    exec(`git clone https://github.com/${owner}/${repo}.git ~/${repo}`)
+    const repoUrl = `https://github.com/${owner}/${repo}.git`
     const postsLocation = path.join(`~`, repo, postsPath)
 
-    core.debug(`Read posts location directory`)
+    debug(`Clone repo: ${repoUrl}`)
+
+    exec(`git clone ${repoUrl} ~/${repo}`)
+
+    debug(`Read posts location directory: ${postsLocation}`)
+    debug(`Test: ${JSON.stringify(await fs.readdir(path.join(`~`, repo)))}`)
 
     const files = await fs.readdir(postsLocation)
     const filesMd = files.filter(fileName => fileName.endsWith('.md'))
 
-    core.debug(`Fetch posts`)
+    debug(`Fetch posts`)
 
     const posts = []
     for (let isAllPagesFetched = false, i = 0; !isAllPagesFetched; i++) {
@@ -64,8 +68,8 @@ async function run() {
       isAllPagesFetched = resp.data.user.publication.posts.length === 0
     }
 
-    core.debug(`Posts: ${JSON.stringify(posts)}`)
-    core.debug(`Files: ${JSON.stringify(filesMd)}`)
+    debug(`Posts: ${JSON.stringify(posts)}`)
+    debug(`Files: ${JSON.stringify(filesMd)}`)
 
     const postsByName = posts.reduce((acc, post) => {
       acc[post.slug] = post
@@ -77,11 +81,11 @@ async function run() {
       if (postName in postsByName) {
         const fileData = (await fs.readFile(path.join(postsLocation, fileName))).toString()
         if (postsByName[postName].contentMarkdown !== fileData) {
-          core.debug(`UPDATE: ${postName}`)
+          debug(`UPDATE: ${postName}`)
         }
-        core.debug(`EXIST: ${postName}`)
+        debug(`EXIST: ${postName}`)
       } else {
-        core.debug(`CREATE: ${postName}`)
+        debug(`CREATE: ${postName}`)
       }
     }
 
